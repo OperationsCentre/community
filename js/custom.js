dd852a18bc3c3f3acb3879a75ef9126c7c7a39ef
joshjh2002 <joshhelman@hotmail.co.uk>
2021-12-15 12:00:44 +0000

    Added Staff Team Section

--- js/custom.js
+++ js/custom.js
@@ -1,25 +1,3 @@
-var rustSererData = 'https://rust-servers.net/api/?object=servers&element=detail&key=5vFm4JoByS3EurCBP03CyMpiPfRitI5aM8q'
-
-$.getJSON(rustSererData, function(data) {
-    var progressbar = document.getElementById("rust-population");
-    var playercount = data.players;
-    var playermax = data.maxplayers;
-    //var playercount = 20;
-    progressbar.value = playercount;
-
-    progressbar.max = playermax;
-
-    var currentplayerstext = playercount + "/" + playermax;
-    progressbar.setAttribute("data-text", currentplayerstext);
-
-    var servername = document.getElementById("rust-servername");
-    servername.innerHTML = data.hostname;
-
-    var rustrank = document.getElementById("rust-rank");
-    var rank = data.rank;
-    rustrank.innerHTML = data.rank;
-});
-
 var conanServerData = 'https://conan-exiles.com/api/?object=servers&element=detail&key=pPErjPgTNAqpdedOFkVcIFYJeF2JXdWYR88'
 
 $.getJSON(conanServerData, function(data) {
@@ -39,28 +17,23 @@ $.getJSON(conanServerData, function(data) {
     var rustrank = document.getElementById("conan-rank");
     var rank = data.rank;
     rustrank.innerHTML = data.rank;
-});
 
-var rustServerDataBattle = 'https://api.battlemetrics.com/servers/12487918'
-
-$.getJSON(rustServerDataBattle, function(data) {
-    var rustfps = document.getElementById("rust-curfps");
-    rustfps.innerHTML = data.data.attributes.details.rust_fps;
-
-    var rustfps = document.getElementById("rust-avgfps");
-    rustfps.innerHTML = data.data.attributes.details.rust_fps_avg;
-    /*
-    var rustfps = document.getElementById("rust-description")
-    var description = data.data.attributes.details.rust_description
-    description = description.replaceAll("\n", "<br>")
-    rustfps.innerHTML = description*/
 });
 
 
+//var discordAPI = 'https://discord.com/api/guilds/651455552517570586/members'
 
-var discordAPI = 'https://discord.com/api/guilds/651455552517570586/widget.json'
+//const containerOriginal = document.getElementById("live_discord").innerHTML;
+/*
+var iMax = 8;
+$.getJSON(discordAPI, function(data) {
+    console.log(data)
 
-const containerOriginal = document.getElementById("live_discord").innerHTML;
+    
+});
+*/
+
+/*
 
 var iMax = 8;
 $.getJSON(discordAPI, function(data) {
@@ -83,7 +56,6 @@ $.getJSON(discordAPI, function(data) {
 
     }
 });
-
 function setMaxUsers() {
     const button = document.getElementById("showmorediscordusers");
     if (iMax === 40) {
@@ -93,7 +65,6 @@ function setMaxUsers() {
         iMax = 40;
         button.innerHTML = "Show Less";
     }
-
 
     $.getJSON(discordAPI, function(data) {
         const container = document.getElementById("live_discord");
@@ -117,3 +88,4 @@ function setMaxUsers() {
         }
     });
 };
+*/
