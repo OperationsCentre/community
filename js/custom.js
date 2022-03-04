17051e03462d6353332eb85123dfdc6f6723751f
joshjh2002 <joshhelman@hotmail.co.uk>
2022-03-04 19:56:07 +0000

    Added Rust Back In

--- js/custom.js
+++ js/custom.js
@@ -1,21 +1,63 @@
-var conanServerData = 'https://conan-exiles.com/api/?object=servers&element=detail&key=pPErjPgTNAqpdedOFkVcIFYJeF2JXdWYR88'
+var conanServerData =
+  "https://conan-exiles.com/api/?object=servers&element=detail&key=pPErjPgTNAqpdedOFkVcIFYJeF2JXdWYR88";
 
-$.getJSON(conanServerData, function(data) {
-    var progressbar = document.getElementById("conan-population");
-    var playercount = data.players;
-    var playermax = data.maxplayers;
-    progressbar.value = playercount;
+$.getJSON(conanServerData, function (data) {
+  var progressbar = document.getElementById("conan-population");
+  var playercount = data.players;
+  var playermax = data.maxplayers;
+  progressbar.value = playercount;
 
-    progressbar.max = playermax;
+  progressbar.max = playermax;
 
-    var currentplayerstext = playercount + "/" + playermax;
-    progressbar.setAttribute("data-text", currentplayerstext);
+  var currentplayerstext = playercount + "/" + playermax;
+  progressbar.setAttribute("data-text", currentplayerstext);
 
-    var servername = document.getElementById("conan-servername");
-    servername.innerHTML = data.hostname;
+  var servername = document.getElementById("conan-servername");
+  servername.innerHTML = data.hostname;
 
-    var rustrank = document.getElementById("conan-rank");
-    var rank = data.rank;
-    rustrank.innerHTML = data.rank;
+  var rustrank = document.getElementById("conan-rank");
+  var rank = data.rank;
+  rustrank.innerHTML = data.rank;
+});
 
+var rustSererData =
+  "https://rust-servers.net/api/?object=servers&element=detail&key=c0T0pgPyaS6EnnU1XuTjFh1rmvJdPQz1gnE";
+
+$.getJSON(rustSererData, function (data) {
+  var progressbar = document.getElementById("rust-population");
+  var playercount = data.players;
+  var playermax = data.maxplayers;
+  //var playercount = 20;
+  progressbar.value = playercount;
+
+  progressbar.max = playermax;
+
+  var currentplayerstext = playercount + "/" + playermax;
+  progressbar.setAttribute("data-text", currentplayerstext);
+
+  var servername = document.getElementById("rust-servername");
+  servername.innerHTML = data.hostname;
+
+  var rustrank = document.getElementById("rust-rank");
+  var rank = data.rank;
+  rustrank.innerHTML = data.rank;
+
+  var uptimeEnt = document.getElementById("rust-uptime");
+  var uptime = data.uptime;
+  uptimeEnt.innerHTML = uptime;
+});
+
+var rustServerDataBattle = "https://api.battlemetrics.com/servers/14481826";
+
+$.getJSON(rustServerDataBattle, function (data) {
+  var rustfps = document.getElementById("rust-curfps");
+  rustfps.innerHTML = data.data.attributes.details.rust_fps;
+
+  var rustfps = document.getElementById("rust-avgfps");
+  rustfps.innerHTML = data.data.attributes.details.rust_fps_avg;
+
+  var lastwipe = document.getElementById("rust-wipe");
+  var wipe = data.data.attributes.details.rust_last_wipe;
+  console.log(wipe);
+  lastwipe.innerHTML = wipe;
 });
